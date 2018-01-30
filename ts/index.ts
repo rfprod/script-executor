1b5cbcb4ee3fa8724fd5d50b64fe434a74ba2dd3
rfprod <zoidenmacht@zoho.com>
2018-01-30 00:57:35 -0800

    (update) index.ts - added colorized output

--- ts/index.ts
+++ ts/index.ts
@@ -1,12 +1,38 @@
+import * as colors from 'colors';
+/*
+*	TEXT COLORS
+*
+*	black
+*	red
+*	green
+*	yellow
+*	blue
+*	magenta
+*	cyan
+*	white
+*	gray
+*	grey
+*/
+
 export module TestModule {
 	export class TestTask {
-		public t: number = 2;
-		public x: number = 10 / this.t;
+
+		private input: any = [
+			{
+				arr: [
+					{ x: 1, y: 1 },
+					{ x: 2, y: 2 }
+				] as any[],
+				expected: [
+					{ x: 1, y: 1 }
+				] as any[]
+			}
+		];
 
 		constructor() {
-			console.log('TestModule: TestTask init');
-			console.log('t', this.t);
-			console.log('x', this.x);
+			console.log(colors.green('TestModule: TestTask init'));
+			console.log(colors.yellow('sample input'), this.input[0].arr);
+			console.log(colors.yellow('sample expected'), this.input[0].expected);
 		}
 	}
 }
