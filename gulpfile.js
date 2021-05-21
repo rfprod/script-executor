b5c9849c751161c46c1a05c257d70ed2944ede14
rfprod <zraw.stdio@gmail.com>
2021-05-21 20:37:20 +0300

    chore(dejunk): remove junk

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,8 @@
 
 const gulp = require('gulp');
 const spawn = require('child_process').spawn;
-const exec = require('child_process').exec;
 
 let node;
-let tsc;
 
 gulp.task('run-js-script', done => {
   if (node) node.kill();
@@ -14,17 +12,19 @@ gulp.task('run-js-script', done => {
   });
   node.on('close', code => {
     if (code === 8) {
-      console.log('Error detected, waiting for changes...');
+      gulp.log('Error detected, waiting for changes...');
     } else {
       done();
     }
   });
 });
 
-gulp.task('tsc', done => {
-  if (tsc) tsc.kill();
-  tsc = spawn('tsc', [], { stdio: 'inherit' });
-  tsc.on('close', code => {
+gulp.task('run-ts-script', done => {
+  if (node) node.kill();
+  node = spawn('ts-node', ['-P', './tsconfig.json', './ts/index.ts'], {
+    stdio: 'inherit',
+  });
+  node.on('close', code => {
     if (code === 8) {
       gulp.log('Error detected, waiting for changes...');
     } else {
@@ -33,25 +33,9 @@ gulp.task('tsc', done => {
   });
 });
 
-gulp.task('run-ts-script', () => {
-  if (node) node.kill();
-  node = spawn('node', ['-e', 'new require("./ts/index.js").TestModule.TestTask()'], {
-    stdio: 'inherit',
-  });
-  node.on('close', code => {
-    if (code === 8) {
-      console.log('Error detected, waiting for changes...');
-    }
-  });
-});
-
-gulp.task('compile-and-run-ts-script', done => {
-  gulp.series('tsc', 'run-ts-script', done);
-});
-
 gulp.task('watch', () => {
   gulp.watch(['./index.js'], gulp.series(['run-js-script'])); // watch js script changes and execute
-  gulp.watch(['./ts/index.ts'], gulp.series(['compile-and-run-ts-script'])); // watch ts script changes and execute
+  gulp.watch(['./ts/index.ts'], gulp.series(['run-ts-script'])); // watch ts script changes and execute
 });
 
 gulp.task('default', gulp.series(['run-js-script', 'watch']));
--- ts/index.ts
+++ ts/index.ts
@@ -143,3 +143,5 @@ export namespace TestModule {
     };
   }
 }
+
+new TestModule.TestTask();
